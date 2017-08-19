a8e23ec21a12c43603769829a526dcb8bf49237b
Chandra <stshivamtripathi7@gmail.com>
2017-08-19 19:25:04 +0530

    Updated all file

--- js/Agriculture.js
+++ js/Agriculture.js
@@ -4,6 +4,8 @@ var res = [];
 var re = [];
 var riceResult = [];
 var commResult = [];
+var year = [];
+var commAggre = [];
 fs.readFile('../csv/Agriculture.csv','UTF-8',function(err,usedData)
 {
   const writeFileOil = fs.createWriteStream('../json/OilSeeds.json');
@@ -14,22 +16,24 @@ fs.readFile('../csv/Agriculture.csv','UTF-8',function(err,usedData)
   var arr=usedData
   .split('\n')
   .map((usedData)=>{
-    return usedData.split(',')
+    return usedData.split(/,(?![^"]*"(?:(?:[^"]*"){2})*[^"]*$)/)
   })
 
+
   for(var i=1;i<arr.length;i++)
-  {   
+  { 
+
    if(arr[i][0].match("Oilseeds")=="Oilseeds")
    {  
-    oil=arr[i][24];
-    oil1="Oilseed";
+    oil=arr[i][23];
+    oil1= arr[i][0];
     var argument =new Object;
     argument.name=oil1
     argument.value = oil
     result.push(argument)
     var invalidEntries = 0;
     function isNumber(obj) {
-      return obj!== undefined && typeof(obj) === 'string' && !isNaN(obj);
+      return obj!== undefined&& typeof(obj) === 'string'  && !isNaN(obj);
     }
     function filterByid(argument) {
       if (isNumber(argument.value)) {
@@ -42,16 +46,17 @@ fs.readFile('../csv/Agriculture.csv','UTF-8',function(err,usedData)
     var x = arrByid.sort(function(a,b){
       return b.value-a.value;
     })
-    var ar2 = [];
+   
     
-ar2 = arrByid.filter((item)=>{return item.name==="Oilseed"}).map((event)=>{return event.value}).reduce((prev, value) => {return prev+parseFloat(value)},0);
-console.log(ar2);
+
 
   }
   if(arr[i][0].match("Production")=="Production")
  {
-  prod =arr[i][24];
+  prod =arr[i][23];
+  prodName = arr[i][0];
   var arg =new Object;
+  arg.name = prodName
   arg.value = prod
   res.push(arg)
   var invalidEntrie = 0;
@@ -73,8 +78,10 @@ console.log(ar2);
 
 if(arr[i][0].match("Foodgrains")=="Foodgrains")
  {
-  food =arr[i][24];
+  food =arr[i][23];
+  foodName = arr[i][0]
   var args =new Object;
+  args.name = foodName
   args.value = food
   re.push(args)
   var invalidEntr = 0;
@@ -98,8 +105,10 @@ if(arr[i][0].match("Foodgrains")=="Foodgrains")
 
 if(arr[i][0].match("Rice")=="Rice")
    {  
-    rice =arr[i][24];
+    rice =arr[i][23];
+    riceName = arr[i][0]
     var item =new Object;
+    item.name = riceName
     item.value = rice
     riceResult.push(item)
     var invalidEnt = 0;
@@ -120,14 +129,23 @@ if(arr[i][0].match("Rice")=="Rice")
   }
 
 if(arr[i][0].match("Commercial")=="Commercial")
-   {  
-    commCrop =arr[i][24];
+   { 
+   
+     j = 15;
+   
+    do
+    {
+    commCrop = arr[i][j];
+    commCrop1 = arr[0][j];
+     year.push(commCrop1);
+    //console.log(year[9])
     var itemComm =new Object;
+    itemComm.name = commCrop1;
     itemComm.value = commCrop;
     commResult.push(itemComm)
     var invalidVal = 0;
     function isNumber(obj) {
-      return obj!== undefined && typeof(obj) === 'string' && !isNaN(obj);
+      return obj!== undefined && typeof(obj) == 'string' && !isNaN(obj);
     }
     function filterByidComm(itemComm) {
       if (isNumber(itemComm.value)) {
@@ -140,17 +158,30 @@ if(arr[i][0].match("Commercial")=="Commercial")
     var commX = arrByidComm.sort(function(a,b){
       return b.value-a.value;
     })
+   
+    commAggre = commX.filter((event) => { return event.name == year[j] })
+   .map((itemComm)=>{return itemComm.value})
+   .reduce((prev, value)=>{return prev + parseFloat(value)}, 0);
+      var commercialAggre = commAggre;
+      console.log(year[j], commAggre)
+      j++;
+      
+    }
+    while(j<arr.length);
+      
   }
 
+
 } 
-writeFileOil.write(JSON.stringify(x, null, 2), 'UTF8');
+//console.log(year);
+writeFileOil.write(JSON.stringify(x, null, 2 ), 'UTF8');
 writeFileProd.write(JSON.stringify(y, null, 2), 'UTF8');
 writeFileFood.write(JSON.stringify(z, null, 2), 'UTF8');
 writeFileRice.write(JSON.stringify(riceX, null, 2), 'UTF8');
-writeFileCommCrop.write(JSON.stringify(commX, null, 2), 'UTF8');
+writeFileCommCrop.write(JSON.stringify(commX, null, 1), 'UTF8');
 
-
-
+  //console.log(commercialAggre)
+ 
 
 })
-
+ 
